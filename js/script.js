e1e3c3e654afeee208748cfd00e34f0ab880f19a
MatteoDiMario96 <dm.matteo96@live.it>
2024-06-05 18:31:09 +0200

    Creata una funzione che mi genera N emails. Grazie a un ciclo for fatto iterare N numero di volte, cosa? L'inter axios.get all'indirizzo ***. Poi quindi restituisci dammi come variabile il response.data.response Poi nell'array risultati pusha risultato.

--- js/script.js
+++ js/script.js
@@ -3,19 +3,24 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            risultati: [],
         }
     },
     methods:{
-        getData: function(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(function (response) {
-                // handle success
-                console.log(response.data.response);
-            })
-        }
+        getNEmails: function(numero){
+            for(let i = 0; i < numero; i++){
+                //Prendi dall'indirizzo i dati.
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                // Ritornami...
+                .then((response) => {
+                    let risultato = response.data.response;
+                    //Per poter chiamare l'array risultati, il this non funzionava perchè la function del then mi cambiava scoop del this. Invece grazie all'arrow function ho potuto prenderlo. 
+                    this.risultati.push(risultato);
+                })
+            }
+        },
     },
     created(){
-        this.getData()
+        this.getNEmails(10)
     }
 }).mount('#app')
